f6dcb12d65fb7c83a804aa347be3687c60575bc6
Niall McGuinness <niall.mcguinness@dkit.ie>
2025-09-29 14:41:43 +0100

    Corrected peaky blinder error

--- IntroToMonoGame/DemoPrimitiveTypePyramid.cs
+++ IntroToMonoGame/DemoPrimitiveTypePyramid.cs
@@ -9,35 +9,36 @@ namespace IntroToMonoGame
         public void InitializeVerts()
         {
             _verts = new VertexPositionColor[16];
-
-            //peak
-            _verts[0] = new VertexPositionColor(
-                new Vector3(0, 1, 0), Color.Red);
-            
+           
             //line 1s-1e - base front
-            _verts[1] = new VertexPositionColor(
+            _verts[0] = new VertexPositionColor(
                 new Vector3(-0.5f, 0, 0.5f), Color.Yellow);
-            _verts[2] = new VertexPositionColor(
+            _verts[1] = new VertexPositionColor(
                new Vector3(0.5f, 0, 0.5f), Color.Yellow);
 
             //line 2s-2e - base right
-            _verts[3] = new VertexPositionColor(
+            _verts[2] = new VertexPositionColor(
                 new Vector3(0.5f, 0, 0.5f), Color.Yellow);
-            _verts[4] = new VertexPositionColor(
+            _verts[3] = new VertexPositionColor(
                new Vector3(0.5f, 0, -0.5f), Color.Yellow);
 
             //line 3s-3e - base back
-            _verts[5] = new VertexPositionColor(
+            _verts[4] = new VertexPositionColor(
                 new Vector3(0.5f, 0, -0.5f), Color.Yellow);
-            _verts[6] = new VertexPositionColor(
+            _verts[5] = new VertexPositionColor(
                new Vector3(-0.5f, 0, -0.5f), Color.Yellow);
 
             //line 4s-4e - base left
-            _verts[7] = new VertexPositionColor(
+            _verts[6] = new VertexPositionColor(
                 new Vector3(-0.5f, 0, -0.5f), Color.Yellow);
-            _verts[8] = new VertexPositionColor(
+            _verts[7] = new VertexPositionColor(
                new Vector3(-0.5f, 0, 0.5f), Color.Yellow);
 
+            //line 5s-5e - front right
+            _verts[8] = new VertexPositionColor(
+                new Vector3(0, 1, 0), Color.Red);
+            _verts[9] = new VertexPositionColor(
+               new Vector3(0.5f, 0, 0.5f), Color.Yellow);
 
 
         }
